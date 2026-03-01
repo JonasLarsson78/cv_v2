2d1ce3eae79cf6eaf0aa5e78e76d9da89abc411b
Jonas L <jl.7804@gmail.com>
2026-03-01 12:25:45 +0100

    hide projects route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import CV from '../components/CV.vue'
-import Projects from '../components/Projects.vue'
+//import Projects from '../components/Projects.vue'
 import PrintCV from '../components/PrintCV.vue'
 
 
 const routes = [
    { path: '/', component: CV },
-   { path: '/projects', component: Projects },
+   //{ path: '/projects', component: Projects },
    {path: '/print', component: PrintCV },
 ]
 
